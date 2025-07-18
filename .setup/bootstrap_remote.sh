b557713aa93b57e255c47e14a7be4cbf13d3ea60
Johann Hofmann <johannhof@chromium.org>
2025-07-18 03:37:20 +0000

    Add tpm to setup

--- .setup/bootstrap_remote.sh
+++ .setup/bootstrap_remote.sh
@@ -12,3 +12,4 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
